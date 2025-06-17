3e1838fa571a7fd4f30141a9b8da7321bbd540e4
Nikita Striuk <32720808+nikitastryuk@users.noreply.github.com>
2025-06-17 16:18:06 +0200

    [UI] Fix - Invalidate nav tree after running SQL (#1120)
    * invalidate nav tree after run sql

--- ui/src/modules/sql-editor/sql-editor-center-panel/sql-editor-center-panel.tsx
+++ ui/src/modules/sql-editor/sql-editor-center-panel/sql-editor-center-panel.tsx
@@ -9,6 +9,7 @@ import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { SQLEditor } from '@/modules/sql-editor/sql-editor';
 import { useSqlEditorPanelsState } from '@/modules/sql-editor/sql-editor-panels-state-provider';
 import { getGetDashboardQueryKey } from '@/orval/dashboard';
+import { getGetNavigationTreesQueryKey } from '@/orval/navigation-trees';
 import { getGetQueriesQueryKey, useCreateQuery } from '@/orval/queries';
 
 import { SqlEditorResizableHandle, SqlEditorResizablePanel } from '../sql-editor-resizable';
@@ -50,6 +51,9 @@ export function SqlEditorCenterPanel() {
           }),
           queryClient.invalidateQueries({
             queryKey: getGetDashboardQueryKey(),
+          }),
+          queryClient.invalidateQueries({
+            queryKey: getGetNavigationTreesQueryKey(),
           }),
         ]);
         if (newQueryRecord) {
